b18bc44706248a11957f0365b7176ebf1f299c0c
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2026-01-24 17:12:37 -0500

    Update

--- public/index.php
+++ public/index.php
@@ -9,10 +9,6 @@ ini_set('display_errors', 1);
  */
 
 use DLCore\Boot\Project;
-use DLRoute\Requests\DLRoute;
-use DLCore\Core\Output\View;
-use DLCore\Parsers\Slug\Path;
-use DLRoute\Server\DLServer;
 
 $sessionExpirte = time() + 1300;
 
@@ -22,34 +18,3 @@ session_start();
 include dirname(__DIR__, 1) . "/vendor/autoload.php";
 
 Project::run();
-
-DLRoute::get(uri: '/', controller: function () {
-    return View::get();
-});
-
-DLRoute::get('/ciencia', function () {
-    return $_SERVER;
-});
-
-DLRoute::get(uri: '/ports', controller: fn() => [
-    "port" => DLServer::get_port(),
-    "local_port" => DLServer::get_local_port(),
-    "url_base" => DLServer::get_base_url()
-], mime_type: 'application/json');
-
-DLRoute::get('/david', function () {
-    return [];
-});
-
-DLRoute::get('/dir', function () {
-
-    return [
-        "one-resolve" => Path::resolve("/mi/ruta"),
-        "resolve" => Path::resolve('/sandbox/../../etc/passwd algo por aquí.md'),
-        "resolve_filename" => Path::resolve_filename('/sandbox/../../etc/passwd algo por aquí.md'),
-        "get_filename" => Path::get_filename("David Eduardo", true),
-        "routes" => DLRoute::get_routes()
-    ];
-});
-
-DLRoute::execute();
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,54 @@
 
 declare(strict_types=1);
 
+use DLCore\Core\Output\View;
+use DLCore\Parsers\Slug\Path;
 use DLRoute\Requests\DLRoute;
+use DLRoute\Server\DLServer;
 
-DLRoute::get('/routes', function() {
+DLRoute::get(uri: '/', controller: function () {
+    return View::get();
+});
+
+DLRoute::get('/routes', function () {
     return [
-        "status" => "Ruta de prueba"
+        "status" => "Una prueba para Leroy",
+        "local_port" => DLServer::get_local_port(),
+        "port" => DLServer::get_port(),
+        "route" => DLServer::get_route(),
+        "dir" => DLServer::get_dir(),
+        "uri" => DLServer::get_uri(),
+        "host" => DLServer::get_host(),
+        "http_host" => DLServer::get_http_host(),
+        "url_base" => DLServer::get_base_url(),
+        "ip" => DLServer::get_ipaddress(),
+    ];
+});
+
+# Lo que existía antes.
+
+
+DLRoute::get('/ciencia', function () {
+    return $_SERVER;
+});
+
+DLRoute::get(uri: '/ports', controller: fn() => [
+    "port" => DLServer::get_port(),
+    "local_port" => DLServer::get_local_port(),
+    "url_base" => DLServer::get_base_url()
+], mime_type: 'application/json');
+
+DLRoute::get('/david', function () {
+    return [0];
+});
+
+DLRoute::get('/dir', function () {
+
+    return [
+        "one-resolve" => Path::resolve("/mi/ruta"),
+        "resolve" => Path::resolve('/sandbox/../../etc/passwd algo por aquí.md'),
+        "resolve_filename" => Path::resolve_filename('/sandbox/../../etc/passwd algo por aquí.md'),
+        "get_filename" => Path::get_filename("David Eduardo", true),
+        "routes" => DLRoute::get_routes()
     ];
 });
